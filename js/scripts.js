fc04b00276d05c3ab07102a0e24c4f82452594e7
Chris Turina, Emily Wells Jimenez and Susannah Lowe <susannah@>
2017-09-26 16:19:50 -0700

    after linas help

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,47 @@ function Ticket(movie, time, age) {
   this.age = age;
 }
 
-Ticket.prototype.ticketCost = function(){
-  
+Ticket.prototype.fullTicket = function() {
+  return this.movie + ", " + this.time + ", " + this.age;
 }
 
+Ticket.prototype.calculateCost = function () {
+  var finalCost = 0;
+  if ((this.movie === "Pokemon") || (this.movie === "James Bond")) {
+    finalCost += 4;
+  }
+  else {
+    finalCost += 6;
+
+  }return finalCost;
+}
+  // if ((this.time === "12-pm") || (this.time === "3-pm")) {
+  //   finalCost += 0;
+  // }  else {
+  //   finalCost += 3;
+  // }
+  // if (this.age === "Adult") {
+  //   finalCost += 2;
+  // }  else if (this.age === "Youth") {
+  //   finalCost += 1;
+  // } else {
+  //   finalCost += 0;
+  // } return;
+
+
+
 //front-end logic
+$(document).ready(function(){
+  $("#movie-ticket").submit(function(event) {
+    event.preventDefault();
+    var movie = $("#movie").val();
+    var time = $("#time").val();
+    var age = $("#age").val();
+    var ticketInfo = new Ticket(movie, time, age);
+    var ticketDisplay = ticketInfo.fullTicket();
+    var ticketCost = ticketInfo.calculateCost();
+    $("#result").text(ticketDisplay);
+    $("#cost").text(ticketCost);
+  })
+
+});
